5efe9ae5123cc20297beb2bfb2f77752575fe1b9
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-11-07 01:11:27 +0300

    m

--- main.py
+++ main.py
@@ -38,30 +38,42 @@ df["age"] = age
 df["gender"] = gender
 
 
-uploaded_files = df
-
 if st.button("Dosyayı Yükle ve Analiz Et"):
-    
-    try:
-        wfa, lhfa = calc(weight,height,age,gender)
-        df["wfa"] = wfa
-        df["lhfa"] = lhfa
-        save_to_log('INFO', 'Dosya işleme işlemi başarılı.')
-        st.success(f"Your wfa is {wfa} anf lhfa is {lhfa}") 
+
+    create_user_log_file()
+
+    if gender:
         
+        save_to_log('INFO', 'Dosya yükleme işlemi başlatıldı.')
+
         try:
-            result = [weight,height,age,gender,wfa,lhfa]
-            save_results(result)
-            save_to_log('INFO', 'Sonuç dosyası oluşturuldu.')
-            save_files()
-            save_to_log('INFO', 'Dosyalar Google Drive\'a yüklendi.')
+            dataframe = df#pd.read_csv(uploaded_files, header=0)
+            save_to_log('INFO', 'Dosya yükleme işlemi başarılı.')
+
+            try:
+                wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
+                df["wfa"] = wfa
+                df["lhfa"] = lhfa
+                save_to_log('INFO', 'Dosya işleme işlemi başarılı.')
+                st.success(f"Your wfa is {wfa} anf lhfa is {lhfa}") #st.success(f"Sonuç: {result}")
+
+                try:
+                    result = [weight,height,age,gender,wfa,lhfa]
+                    save_results(result)
+                    save_to_log('INFO', 'Sonuç dosyası oluşturuldu.')
+                    save_files()
+                    save_to_log('INFO', 'Dosyalar Google Drive\'a yüklendi.')
+
+                except Exception as e:
+                    save_to_log('ERROR', 'Sonuç dosyası oluşturulamadı.')
+                    save_to_log('ERROR', e)
+
+            except Exception as e:
+                save_to_log('ERROR', 'Dosya işleme işlemi başarısız.')
+                save_to_log('ERROR', e)
+                st.write("Dosya işleme işlemi başarısız.")
 
         except Exception as e:
-            save_to_log('ERROR', 'Sonuç dosyası oluşturulamadı.')
+            save_to_log('ERROR', 'Dosya yükleme işlemi başarısız.')
             save_to_log('ERROR', e)
-    
-    except Exception as e:
-        save_to_log('ERROR', 'Dosya işleme işlemi başarısız.')
-        save_to_log('ERROR', e)
-        st.write("Dosya işleme işlemi başarısız.")
-        
+            st.write("Dosya yüklenirken bir hata oluştu.")
