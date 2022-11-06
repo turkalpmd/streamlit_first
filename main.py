6f99e3e9e32770d888549f9e2dc115bc850b357e
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-11-07 01:17:28 +0300

    m

--- main.py
+++ main.py
@@ -40,40 +40,24 @@ df["gender"] = gender
 
 if st.button("Dosyayı Yükle ve Analiz Et"):
 
-    create_user_log_file()
-
-    if gender:
+    try:
+        wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
+        df["wfa"] = wfa
+        df["lhfa"] = lhfa
         
-        save_to_log('INFO', 'Dosya yükleme işlemi başlatıldı.')
+        st.success(f"Your wfa is {wfa} anf lhfa is {lhfa}") #st.success(f"Sonuç: {result}")
 
         try:
-            dataframe = df#pd.read_csv(uploaded_files, header=0)
-            save_to_log('INFO', 'Dosya yükleme işlemi başarılı.')
-
-            try:
-                wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
-                df["wfa"] = wfa
-                df["lhfa"] = lhfa
-                save_to_log('INFO', 'Dosya işleme işlemi başarılı.')
-                st.success(f"Your wfa is {wfa} anf lhfa is {lhfa}") #st.success(f"Sonuç: {result}")
-
-                try:
-                    result = [weight,height,age,gender,wfa,lhfa]
-                    save_results(result)
-                    save_to_log('INFO', 'Sonuç dosyası oluşturuldu.')
-                    save_files()
-                    save_to_log('INFO', 'Dosyalar Google Drive\'a yüklendi.')
-
-                except Exception as e:
-                    save_to_log('ERROR', 'Sonuç dosyası oluşturulamadı.')
-                    save_to_log('ERROR', e)
-
-            except Exception as e:
-                save_to_log('ERROR', 'Dosya işleme işlemi başarısız.')
-                save_to_log('ERROR', e)
-                st.write("Dosya işleme işlemi başarısız.")
+            result = [weight,height,age,gender,wfa,lhfa]
+            save_results(result)
+            save_files()
+            
 
         except Exception as e:
-            save_to_log('ERROR', 'Dosya yükleme işlemi başarısız.')
+            save_to_log('ERROR', 'Sonuç dosyası oluşturulamadı.')
             save_to_log('ERROR', e)
-            st.write("Dosya yüklenirken bir hata oluştu.")
+
+    except Exception as e:
+        save_to_log('ERROR', 'Dosya işleme işlemi başarısız.')
+        save_to_log('ERROR', e)
+        st.write("Dosya işleme işlemi başarısız.")
