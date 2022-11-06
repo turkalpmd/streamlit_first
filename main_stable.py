33947b48a8a57f2d5e59f03a347aa6f24af933a9
Turkalp MD <85236337+turkalpmd@users.noreply.github.com>
2022-11-07 01:55:14 +0300

    m

---
+++ main_stable.py
@@ -0,0 +1,59 @@
+
+from PIL import Image
+import streamlit as st
+from utils import *
+import pandas as pd
+import numpy as np
+
+
+st.title("Z-score Hesaplama Uygulaması")
+
+img = Image.open("hacettepe.jpg")
+st.image(img)
+
+# Introduction
+
+st.subheader("Büyüme Takibi")
+
+
+st.text("""
+Bu uygulamada Z score'lar 2 yaş ve altında WHO üstünde ise \n
+CDC referans aralıkları kullanılarak hesaplanır.
+	""")
+
+
+weight = st.number_input("KG cinsinden ağırlık;", step = 0.1)
+height = st.number_input("Santimetre cinsinden boyu;")
+age = st.number_input("Ay olarak yaşı;",step=1)
+gender =st.radio("Cinsiyeti seçiniz", options=["Erkek","Kız"])
+    #text_input("Erkek için E Kız için K yazınız;")
+
+if gender == "Erkek":
+    gender = "M"
+if gender == "Kız":
+    gender = "F"
+
+df = pd.DataFrame()
+df["weight"] = weight
+df["height"] = height
+df["age"] = age
+df["gender"] = gender
+
+uploaded_files = df
+
+
+if st.button("Z skorunu Analiz Et"):
+
+
+
+    wfa, lhfa = calc(weight,height,age,gender)#result = process_csv(dataframe)
+    df["wfa"] = wfa
+    df["lhfa"] = lhfa
+    
+    st.success(f"Yaşa göre ağırlık; {wfa}") 
+    st.success(f"Yaşa göre boy; {lhfa}") #st.success(f"Sonuç: {result}")
+    
+    #result = [weight,height,age,gender,wfa,lhfa]
+    #save_results(result)
+    #save_files()
+    
